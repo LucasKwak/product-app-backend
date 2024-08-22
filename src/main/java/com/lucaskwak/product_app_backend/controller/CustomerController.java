541e745c33d71432311aadc5abf4a9cec15403e6
LucasKwak <lkwakiglesia@gmail.com>
2024-08-22 19:51:18 +0200

    [COOKIES] Modify the logic of returning the jwt due to cookies.

--- src/main/java/com/lucaskwak/product_app_backend/controller/CustomerController.java
+++ src/main/java/com/lucaskwak/product_app_backend/controller/CustomerController.java
@@ -3,6 +3,9 @@ package com.lucaskwak.product_app_backend.controller;
 import com.lucaskwak.product_app_backend.security.dto.in.UserDto;
 import com.lucaskwak.product_app_backend.security.dto.out.RegisteredUser;
 import com.lucaskwak.product_app_backend.security.service.AuthenticationService;
+import com.lucaskwak.product_app_backend.security.service.util.RegisteredUserWithJwt;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +26,21 @@ public class CustomerController {
     }
 
     @PostMapping
-    public  ResponseEntity<RegisteredUser> registerOne (@RequestBody UserDto saveUser) {
-        RegisteredUser registeredUser = authenticationService.registerOneCustomer(saveUser);
+    public  ResponseEntity<RegisteredUser> registerOne (@RequestBody UserDto saveUser, HttpServletResponse httpServletResponse) {
+
+        RegisteredUserWithJwt registeredUserWithJwt = authenticationService.registerOneCustomer(saveUser);
+
+        String jwt = registeredUserWithJwt.getJwt();
+        RegisteredUser registeredUser = registeredUserWithJwt.getRegisteredUser();
+
+        Cookie cookie = new Cookie("JWT_TOKEN", jwt);
+        cookie.setHttpOnly(true);  // Previene el acceso desde JavaScript en el navegador
+        cookie.setSecure(false);    // Solo se envía a través de conexiones HTTPS
+        cookie.setPath("/");       // Hace que la cookie esté disponible en toda la aplicación
+        cookie.setMaxAge(24 * 60 * 60);  // Duración de la cookie (1 día en este caso)
+
+        // Agrega la cookie a la respuesta
+        httpServletResponse.addCookie(cookie);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
     }
