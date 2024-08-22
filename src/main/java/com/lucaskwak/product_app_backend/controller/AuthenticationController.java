22e8b3ff1cb3384f6bea95078ed2d7d12b7d98af
LucasKwak <lkwakiglesia@gmail.com>
2024-08-22 19:55:30 +0200

    [COOKIES] Modify the logic of obtaining the jwt due to cookies

--- src/main/java/com/lucaskwak/product_app_backend/controller/AuthenticationController.java
+++ src/main/java/com/lucaskwak/product_app_backend/controller/AuthenticationController.java
@@ -5,7 +5,9 @@ import com.lucaskwak.product_app_backend.security.dto.out.AuthenticationResponse
 import com.lucaskwak.product_app_backend.security.dto.out.LogoutResponse;
 import com.lucaskwak.product_app_backend.security.dto.out.ProfileUser;
 import com.lucaskwak.product_app_backend.security.service.AuthenticationService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,15 +31,42 @@ public class AuthenticationController {
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> authenticate (
-            @RequestBody AuthenticationRequest authenticationRequest) {
+    public ResponseEntity<Void> authenticate (
+            @RequestBody AuthenticationRequest authenticationRequest, HttpServletResponse httpServletResponse) {
         AuthenticationResponse response = authenticationService.login(authenticationRequest);
-        return ResponseEntity.ok(response);
+
+        String jwt = response.getJwt();
+
+        Cookie cookie = new Cookie("JWT_TOKEN", jwt);
+        cookie.setHttpOnly(true);  // Previene el acceso desde JavaScript en el navegador
+        cookie.setSecure(false);    // Solo se envía a través de conexiones HTTPS
+        cookie.setPath("/");       // Hace que la cookie esté disponible en toda la aplicación
+        cookie.setMaxAge(24 * 60 * 60);  // Duración de la cookie (1 día en este caso)
+
+        // Agrega la cookie a la respuesta
+        httpServletResponse.addCookie(cookie);
+
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<LogoutResponse> logout (HttpServletRequest request) {
-        authenticationService.logout(request);
+    public ResponseEntity<LogoutResponse> logout (HttpServletRequest httpServletRequest,
+                                                  HttpServletResponse httpServletResponse) {
+        authenticationService.logout(httpServletRequest);
+
+        Cookie cookie = new Cookie("JWT_TOKEN", "");
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        httpServletResponse.addCookie(cookie);
+
         return ResponseEntity.ok(new LogoutResponse("Se ha cerrado correctamente su sesion"));
+    }
+
+    @GetMapping("/validate-token")
+    public ResponseEntity<Boolean> validate (HttpServletRequest httpServletRequest) {
+        boolean isTokenValid = authenticationService.validateToken(httpServletRequest);
+        return ResponseEntity.ok(isTokenValid);
     }
 }
