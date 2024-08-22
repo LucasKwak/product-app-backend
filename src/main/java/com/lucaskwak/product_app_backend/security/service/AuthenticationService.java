0006b62e5c9308d48f9d612c5c6be9ff0e9fc579
LucasKwak <lkwakiglesia@gmail.com>
2024-08-22 19:43:56 +0200

    [OAUTH2][COOKIES] Add the validate-token operation

--- src/main/java/com/lucaskwak/product_app_backend/security/service/AuthenticationService.java
+++ src/main/java/com/lucaskwak/product_app_backend/security/service/AuthenticationService.java
@@ -4,16 +4,18 @@ import com.lucaskwak.product_app_backend.security.dto.in.AuthenticationRequest;
 import com.lucaskwak.product_app_backend.security.dto.in.UserDto;
 import com.lucaskwak.product_app_backend.security.dto.out.AuthenticationResponse;
 import com.lucaskwak.product_app_backend.security.dto.out.ProfileUser;
-import com.lucaskwak.product_app_backend.security.dto.out.RegisteredUser;
+import com.lucaskwak.product_app_backend.security.service.util.RegisteredUserWithJwt;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface AuthenticationService {
 
-    RegisteredUser registerOneCustomer(UserDto saveUser);
+    RegisteredUserWithJwt registerOneCustomer(UserDto saveUser);
 
     AuthenticationResponse login(AuthenticationRequest authenticationRequest);
 
     ProfileUser findLoggedInUser();
 
-    void logout(HttpServletRequest request);
+    void logout(HttpServletRequest httpServletRequest);
+
+    Boolean validateToken(HttpServletRequest httpServletRequest);
 }
